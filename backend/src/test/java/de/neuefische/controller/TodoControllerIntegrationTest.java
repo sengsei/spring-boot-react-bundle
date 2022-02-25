a2d5011c80014b941114f0cfbfa1aaf5264521c5
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-02-25 08:02:28 +0100

    integration test and service test

--- backend/src/test/java/de/neuefische/controller/TodoControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/controller/TodoControllerIntegrationTest.java
@@ -1,49 +1,59 @@
 package de.neuefische.controller;
 
 import de.neuefische.TodoElement;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Objects;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class TodoControllerIntegrationTest {
+public class TodoControllerIntegrationTest {
 
     @Autowired
-    private TestRestTemplate testRestTemplate;
-
-
-    @Test
-    void shouldAddTodo() {
-        TodoElement todoElement = new TodoElement();
-        todoElement.setTitle("Sport");
-
-        testRestTemplate.postForEntity("/todos", todoElement, TodoElement[].class);
-
-        ResponseEntity<TodoElement> response = testRestTemplate.getForEntity("/todos/Sport", TodoElement.class);
-        assertEquals(todoElement.getId(), Objects.requireNonNull(response.getBody()).getId());
-
-    }
-
+    private TestRestTemplate restTemplate;
 
     @Test
-    void shouldGetElementByName() {
-        TodoElement tE2 = new TodoElement();
-        tE2.setTitle("Java");
+    void integrationTest() {
 
-        testRestTemplate.postForEntity("/todos", tE2, TodoElement.class);
+        TodoElement todo1 = new TodoElement("Sport");
 
-        ResponseEntity<TodoElement> response = testRestTemplate.getForEntity("/todos/Java", TodoElement.class);
-        assertEquals(tE2.getTitle(), Objects.requireNonNull(response.getBody()).getTitle());
+        ResponseEntity<TodoElement[]> response1 = restTemplate.postForEntity("/todos", todo1, TodoElement[].class);
+
+        assertThat(response1.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        Assertions.assertThat(response1.getBody()).containsExactlyInAnyOrder(todo1);
+
+
+        TodoElement todo2 = new TodoElement("Java");
+
+        ResponseEntity<TodoElement[]> response2 = restTemplate.postForEntity("/todos", todo2, TodoElement[].class);
+
+        assertThat(response2.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response2.getBody()).containsExactlyInAnyOrder(todo1, todo2);
+
+
+        restTemplate.delete("/todos/" + todo1.getId());
+        ResponseEntity<TodoElement[]> response3 = restTemplate.getForEntity("/todos", TodoElement[].class);
+
+        assertThat(response3.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response3.getBody()).containsExactlyInAnyOrder(todo2);
+
+
+        TodoElement todoWithChanges = new TodoElement();
+        todoWithChanges.setId(todo2.getId());
+        todoWithChanges.setTitle("Java");
+        todoWithChanges.setText(todo2.getText());
+        todoWithChanges.setState(todo2.getState());
+
+        restTemplate.put("/todos/" + todo2.getId(), todoWithChanges);
+        ResponseEntity<TodoElement[]> response4 = restTemplate.getForEntity("/todos", TodoElement[].class);
+
+        assertThat(response4.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response4.getBody()).containsExactlyInAnyOrder(todoWithChanges);
 
     }
-
-
-
 }
--- backend/src/test/java/de/neuefische/service/TodoServiceTest.java
+++ backend/src/test/java/de/neuefische/service/TodoServiceTest.java
@@ -1,39 +1,104 @@
 package de.neuefische.service;
 
 import de.neuefische.TodoElement;
+import de.neuefische.TodoState;
 import de.neuefische.repository.TodoRepo;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.Collection;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TodoServiceTest {
 
     @Test
-    void getTodoList() {
-        TodoElement todoElement = new TodoElement();
-        todoElement.setTitle("Sport");
-        TodoRepo todoRepo = new TodoRepo();
-        todoRepo.save(todoElement);
-        TodoService todoService = new TodoService(todoRepo);
+    void shouldAddNewTodo() {
+        TodoElement todo1 = new TodoElement();
+        todo1.setTitle("Sport");
+        todo1.setState(TodoState.Open);
 
-        List<TodoElement> expected = List.of(todoElement);
+        TodoRepo repo = Mockito.mock(TodoRepo.class);
+        TodoService todoService = new TodoService(repo);
 
-        assertEquals(expected, todoService.getTodoList());
+        todoService.addTodo(todo1);
+
+        Mockito.verify(repo).save(todo1);
     }
 
     @Test
-    void addTodo() {
-        TodoElement todoElement = new TodoElement();
-        todoElement.setTitle("Sport");
-        TodoRepo todoRepo = new TodoRepo();
-        todoRepo.save(todoElement);
-        TodoService todoService = new TodoService(todoRepo);
+    void shouldRetrieveAllTodos() {
+        TodoElement todo1 = new TodoElement();
+        todo1.setTitle("Sport");
+        todo1.setState(TodoState.Open);
 
-        List<TodoElement> expected = List.of(todoElement);
+        TodoElement todo2 = new TodoElement();
+        todo2.setTitle("Java");
+        todo2.setState(TodoState.Open);
 
-        assertEquals(expected, todoService.getTodoList());
+        List<TodoElement> todoList = List.of(todo1, todo2);
+        TodoRepo repo = Mockito.mock(TodoRepo.class);
+        Mockito.when(repo.getTodoList()).thenReturn(todoList);
+
+        TodoService todoService = new TodoService(repo);
+
+        Collection<TodoElement> actual = todoService.getTodoList();
+
+        assertThat(actual).isEqualTo(todoList);
     }
+
+    @Test
+    void shouldRetrieveOneTodo() {
+        TodoElement todo1 = new TodoElement();
+        todo1.setTitle("Sport");
+        todo1.setState(TodoState.Open);
+
+        TodoRepo repo = Mockito.mock(TodoRepo.class);
+        Mockito.when(repo.getTodoElementByID(todo1.getId())).thenReturn(todo1);
+
+        TodoService todoService = new TodoService(repo);
+
+        TodoElement actual = todoService.getTodoElementById(todo1.getId());
+
+        assertThat(actual).isEqualTo(todo1);
+    }
+
+    @Test
+    void shouldDeleteTodo() {
+        String id = "777";
+
+        TodoRepo repo = Mockito.mock(TodoRepo.class);
+        TodoService todoService = new TodoService(repo);
+
+        todoService.deleteTodo(id);
+
+        Mockito.verify(repo).delete(id);
+    }
+
+    @Test
+    void shouldChangeTodo() {
+        TodoElement todo1 = new TodoElement();
+        todo1.setId("777");
+        todo1.setTitle("Sport");
+        todo1.setState(TodoState.Open);
+
+        TodoElement savedTodo = new TodoElement();
+        savedTodo.setId("777");
+        savedTodo.setTitle("Sport");
+        savedTodo.setState(TodoState.Done);
+
+        TodoRepo repo = Mockito.mock(TodoRepo.class);
+        Mockito.when(repo.getTodoElementByID("777")).thenReturn(todo1);
+
+        TodoService todoService = new TodoService(repo);
+
+        todoService.changeTodo("777", savedTodo);
+
+        Mockito.verify(repo).save(savedTodo);
+    }
+
+
 
 }
