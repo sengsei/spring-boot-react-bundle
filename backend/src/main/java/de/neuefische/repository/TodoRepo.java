95e70776b0861227602f6d22c9de6cc310e76c41
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-03-12 08:01:56 +0100

    RouterForwarding added

--- backend/src/main/java/de/neuefische/repository/TodoRepo.java
+++ backend/src/main/java/de/neuefische/repository/TodoRepo.java
@@ -4,30 +4,7 @@ import de.neuefische.TodoElement;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
-
 @Repository
 public interface TodoRepo extends MongoRepository<TodoElement, String> {
-
-
-    /*
-    private final Map<String, TodoElement> todoList = new HashMap<>();
-
-    public Collection<TodoElement> getTodoList(){
-        return todoList.values();
-    }
-
-    public TodoElement getTodoElementByID(String id){
-        return todoList.get(id);
-    }
-
-    public void save(TodoElement todoElement) {
-        todoList.put(todoElement.getId(), todoElement);
-    }
-
-    public void delete(String id){
-        todoList.remove(id);
-    }
-    */
 
 }
