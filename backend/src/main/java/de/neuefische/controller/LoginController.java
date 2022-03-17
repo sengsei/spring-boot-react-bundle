cacad8e47d87ff59f29c39a3e765d0bb4deba0df
sengsei <sebastian.sengbusch@stud.th-luebeck.de>
2022-03-17 15:06:43 +0100

    JWT, Login, props

---
+++ backend/src/main/java/de/neuefische/controller/LoginController.java
@@ -0,0 +1,50 @@
+package de.neuefische.controller;
+
+import de.neuefische.model.LoginData;
+import de.neuefische.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth/login")
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    @PostMapping
+    public String login(@RequestBody LoginData data){
+        try {
+            Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    data.getUsername(), data.getPassword()
+            ));
+
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("roles", getRoles(auth));
+            return jwtService.createToken(claims, data.getUsername());
+
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "invalid credentials");
+        }
+    }
+    private List<String> getRoles(Authentication auth){
+        return auth.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority)
+                .toList();
+    }
+
+}
---
+++ backend/src/main/java/de/neuefische/model/LoginData.java
@@ -0,0 +1,9 @@
+package de.neuefische.model;
+
+import lombok.Data;
+
+@Data
+public class LoginData {
+    private String username;
+    private String password;
+}
